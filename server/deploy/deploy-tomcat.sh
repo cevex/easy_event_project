c935d8a67b3a5728df58aab575b6ebe75915ffb9
cevex <cevex.dev@gmail.com>
2017-05-14 13:29:18 +0200

    Move tomcat outside + init read me

--- server/deploy/deploy-tomcat.sh
+++ server/deploy/deploy-tomcat.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 warOrigin=~/workspace/EasyEvent/easy_event_project/server/target/easyevent-rest-server.war
-deployTarget=~/workspace/EasyEvent/easy_event_project/server/tomcat-9.0/webapps
+deployTarget=/servers/tomcat-9.0/webapps
 
 cp -rf $warOrigin $deployTarget
